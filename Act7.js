e5064a1ae48787831950bc538f19d4c13b33b556
Miss. C Olckers <u18013695@up.ac.za>
2019-05-07 12:02:19 +0200

    Christine se veranderinge is klaar

--- Act7.js
+++ Act7.js
@@ -2,3 +2,8 @@ function Message(){
 	var message = "Coding is fun";
 	alert(message);
 }
+
+function Christine(){
+	var message = "This isn't INF 354. INF 272 lets you be creative";
+	alert(message);
+}
